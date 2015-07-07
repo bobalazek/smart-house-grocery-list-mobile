c0e2b84ceee199516db4b8f2ac00c0b6ff784a7d
Borut Balazek <bobalazek124@gmail.com>
2015-07-07 18:22:50 +0200

    Preparations for buy mode in progress

--- www/app/modules/buy-mode/buy-mode.js
+++ www/app/modules/buy-mode/buy-mode.js
@@ -19,9 +19,21 @@ angular
         function BuyModeController($rootScope, $scope, $ionicPlatform, $cordovaBarcodeScanner, IS_DESKTOP) {
             var vm = this;
 
+            vm.product = {
+                name: '',
+                description: '',
+                eanCode: '',
+                price: 1.99,
+                quantity: 1,
+                nonExpiring: false, // We'll probably use this varible ...
+                expires: true, // ... and reverse this one on send / save
+                timeExpiring: new Date(),
+            };
+
             vm.scan = function() {
                 if(IS_DESKTOP) {
-                    alert('This functionality does NOT work on desktop!')
+                    alert('This functionality does NOT work on desktop!');
+
                     return false;
                 }
 
@@ -30,13 +42,22 @@ angular
                         .scan()
                         .then(
                             function(barcodeData) {
-
-                            }, function(error) {
+                                // TO-DO: Request on the server and check if the product already exists (to hydrate the data)
+                            },
+                            function(error) {
                                 alert(error);
                             }
                         )
                     ;
                 });
+            };
+
+            vm.save = function() {
+
+            };
+
+            vm.saveAndNew = function() {
+
             };
 
             return vm;
